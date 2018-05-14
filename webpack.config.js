56ed257a55b14e6ef91ff85ec9f452462570e701
Julien <julien.alexandre26@gmail.com>
2018-05-14 17:31:17 +0200

    Installation de Webpack et MomentJS.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './js/index.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+
+    mode: 'development',
+};
