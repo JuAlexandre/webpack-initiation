32771ed18c18dec741e3879213044ce707856336
Julien <julien.alexandre26@gmail.com>
2018-05-14 18:30:20 +0200

    Prise en charge de Bootstrap.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 import moment from "moment";
+import 'bootstrap';
+import '../src/css/style.scss';
+
 let $ = require("jquery");
 
 console.log("Bienvenue dans WebPack !");
 console.log(moment().startOf('day').fromNow());
-console.log($);
+
+$('.btn-place').append(
+    '<button class="btn btn-primary">Le jQuery c\'est super !</button>'
+);
